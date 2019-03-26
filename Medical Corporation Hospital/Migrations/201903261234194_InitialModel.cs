9d2b9e4a48c81fba1aae7bcbb334a4028150baac
kgothatsotlaka <mankgwanyanetlaka@gmail.com>
2019-03-26 14:46:22 +0200

    Start Of EF
    1. Create DbContext Class
       A. DO the connectingStringThing
    2. Enabled Migrations
       Command: Enable-Migrations
    3.

---
+++ Medical Corporation Hospital/Migrations/201903261234194_InitialModel.cs
@@ -0,0 +1,16 @@
+namespace Medical_Corporation_Hospital.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Medical Corporation Hospital/Models/HospitalDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Medical_Corporation_Hospital.Models
+{
+    public class HospitalDbContext: DbContext
+    {
+        public HospitalDbContext()
+            : base("name=MCH_DB_ConnectingString")
+        {
+        }
+//        public virtual  DbSet<Bed> Beds { get; set; }
+//        public virtual  DbSet<City> Cities { get; set; }
+//        public virtual  DbSet<Doctor> Doctors { get; set; }
+//        public virtual  DbSet<Hospital> Hospitals { get; set; }
+//        public virtual  DbSet<Patient> Patients { get; set; }
+//        public virtual  DbSet<Ward> Wards { get; set; }
+
+    }
+}
