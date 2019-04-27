5a3c8c60dd7fd3b9c5a6c24b08ec8fa7637ee2d1
kgothatsotlaka <mankgwanyanetlaka@gmail.com>
2019-04-27 04:29:21 +0200

    Scaffolding
    testing The Scaffolding functionality  in MVC

---
+++ Medical Corporation Hospital/Controllers/HospitalsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Medical_Corporation_Hospital.Core.Domain;
+using Medical_Corporation_Hospital.Persistence;
+
+namespace Medical_Corporation_Hospital.Controllers
+{
+    public class HospitalsController : Controller
+    {
+        private HospitalDbContext db = new HospitalDbContext();
+
+        // GET: Hospitals
+        public ActionResult Index()
+        {
+            var hospitals = db.Hospitals.Include(h => h.City);
+            return View(hospitals.ToList());
+        }
+
+        // GET: Hospitals/Details/5
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hospital hospital = db.Hospitals.Find(id);
+            if (hospital == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hospital);
+        }
+
+        // GET: Hospitals/Create
+        public ActionResult Create()
+        {
+            ViewBag.CityId = new SelectList(db.Cities, "Id", "Name");
+            return View();
+        }
+
+        // POST: Hospitals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,CityId")] Hospital hospital)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Hospitals.Add(hospital);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityId = new SelectList(db.Cities, "Id", "Name", hospital.CityId);
+            return View(hospital);
+        }
+
+        // GET: Hospitals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hospital hospital = db.Hospitals.Find(id);
+            if (hospital == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityId = new SelectList(db.Cities, "Id", "Name", hospital.CityId);
+            return View(hospital);
+        }
+
+        // POST: Hospitals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,CityId")] Hospital hospital)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hospital).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CityId = new SelectList(db.Cities, "Id", "Name", hospital.CityId);
+            return View(hospital);
+        }
+
+        // GET: Hospitals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hospital hospital = db.Hospitals.Find(id);
+            if (hospital == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hospital);
+        }
+
+        // POST: Hospitals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Hospital hospital = db.Hospitals.Find(id);
+            db.Hospitals.Remove(hospital);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
